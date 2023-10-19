1cb9bfed60cec31906664a8f9e1af063dccd2c81
Is3kai <elijah.newcome@mymail.champlain.edu>
2023-10-19 14:35:49 -0400

    Encoded basic variables.

--- untitled6/main.cpp
+++ untitled6/main.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << std::endl;
+    string name, race;
+    int INT, STR, WIS, CHA, CON, DEX, MP, MMP, HP, MHP, XP, XPR;
+    
+
     return 0;
 }
