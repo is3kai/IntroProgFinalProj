4596a9a800aca21e0e94f4f9687a0325cf3d5811
Is3kai <elijah.newcome@mymail.champlain.edu>
2023-10-19 14:46:06 -0400

    New Code Update

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int levelup(float xp, float xpr){
+    if (xp > xpr){
+        xp = xp-xpr;
+        xpr *= 1.5;
+    }else{
+        return -1;
+    }
+}
+
+
+
 int main() {
     string name, race;
-    int INT, STR, WIS, CHA, CON, DEX, MP, MMP, HP, MHP, XP, XPR;
+    int INT, STR, WIS, CHA, CON, DEX;
+    float MP, MMP, HP, MHP, XP, XPR;
     
 
     return 0;
